d42f55d7b0f89e530b72f9d7b1583e6b820644fd
elskow <elskow@outlook.com>
2024-08-31 14:24:14 +0700

    feat: add class obfuscator, for a experimental thing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import utwm from 'unplugin-tailwindcss-mangle/vite';
 import { defineConfig } from 'vite';
 import Icons from 'unplugin-icons/vite';
 import contentCollections from '@content-collections/vite';
-
 
 export default defineConfig({
 	plugins: [
@@ -11,6 +11,7 @@ export default defineConfig({
 		Icons({
 			compiler: 'svelte',
 			autoInstall: true
-		})
+		}),
+		utwm({})
 	]
 });
