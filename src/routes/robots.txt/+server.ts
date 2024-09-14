5ef98d68647a686b499301c8bf0d5f09bb6f2c42
elskow <elskow@outlook.com>
2024-09-14 19:00:38 +0700

    fix: wrong path sitemap

--- src/routes/robots.txt/+server.ts
+++ src/routes/robots.txt/+server.ts
@@ -1,17 +1,17 @@
 export const prerender = true;
 
 export async function GET(): Promise<Response> {
-	// prettier-ignore
-	const body = [
-		'User-agent: *',
-		'Allow: /',
-		'',
-		`Sitemap: ${import.meta.env.VITE_APP_URL ?? `${import.meta.env.DEV ? 'http://localhost:5173' : 'https://localhost:4173'}`}/sitemap.xml`
-	].join('\n').trim();
+    // prettier-ignore
+    const body = [
+        'User-agent: *',
+        'Allow: /',
+        '',
+        `Sitemap: ${import.meta.env.DEV ? 'http://localhost:5173' : 'https://helmyl.com'}/sitemap.xml`
+    ].join('\n').trim();
 
-	const headers = {
-		'Content-Type': 'text/plain'
-	};
+    const headers = {
+        'Content-Type': 'text/plain'
+    };
 
-	return new Response(body, { headers });
+    return new Response(body, {headers});
 }
